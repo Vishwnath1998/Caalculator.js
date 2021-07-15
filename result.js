75b857f3cdc7a470970a5568a753b7b8d4bbfe81
Vishwnath@1998 <telangvishwnath@1998gmail.com>
2021-07-15 15:39:50 +0530

    first

--- result.js
+++ result.js
@@ -1,6 +1,5 @@
 function update(value){
-    document.getElementById("result").innerHTML+=value
-
+    document.getElementById("result").innerHTML+=value;
 }
 function solve(){
     let exp = document.getElementById("result").innerHTML;
@@ -9,7 +8,6 @@ function solve(){
 function erase(){
     document.getElementById("result").innerHTML="";
 }
-
 function reset(){
     let exp = document.getElementById("result").innerHTML;
     let result = exp.substring(0 ,exp.length - 1);
